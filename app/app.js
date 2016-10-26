74f8dbfafa463fd151ecaa3f5ac0b4e7346fe4bd
w <w>
2016-10-26 23:21:45 +0530

    Add data file with read function

--- app/app.js
+++ app/app.js
@@ -1,9 +1,34 @@
-var http = require("http");
-var PORT = 3000;
-var server = http.createServer(function(req,res){
+var express = require("express");
+var app = express();
+var dataFile = require("./data/data.json");
+app.set('port',process.env.PORT || 3000)
+var PORT = app.get("port");
+app.get("/",function(req,res){
+  var info = '';
+  dataFile.speakers.forEach(function(data){
+    info +=`
+      <li>
+        <h1>${data.name}</h1>
+        <p>${data.summary}</p>
+      </li>
+    `
+  })
+  res.send(`
+      <h1>Node Work With</h1>
+      ${info}
+      `);
+})
 
-  res.writeHead(200,{"Content-Type":"text/plain"});
-  res.write("Node Rocks");
-  res.end();
-}).listen(3000);
-console.log("Server running on : "+PORT);
+
+app.listen(PORT,function(){
+  console.log("Server is Running on: "+PORT);
+})
+
+// var http = require("http");
+// var server = http.createServer(function(req,res){
+//
+//   res.writeHead(200,{"Content-Type":"text/plain"});
+//   res.write("Node Rocks");
+//   res.end();
+// }).listen(3000);
+// console.log("Server running on : "+PORT);
