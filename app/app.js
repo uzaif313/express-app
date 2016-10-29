423e9c628e4137517a722b7f8d6198789ad51748
w <w>
2016-10-29 07:41:21 +0530

    Artwork add

--- app/app.js
+++ app/app.js
@@ -2,10 +2,16 @@ var express = require("express");
 var reload = require("reload");
 var app = express();
 var dataFile = require("./data/data.json");
+
 app.set('port',process.env.PORT || 3000)
 var PORT = app.get("port");
-app.use(express.static("app/public"));
 app.set("appData",dataFile);
+app.set("view engine","ejs");
+app.set("views","app/views")
+
+app.locals.siteTitle = "Ahmedabad Meetups"
+
+app.use(express.static("app/public"));
 app.use(require("./routes/index"));
 app.use(require("./routes/speakers"));
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,20 @@
 var express = require("express");
 var router = express.Router();
 router.get("/",function(req,res){
-  res.send(`<h1>Welcome to Community</h1>
-  			<img src="/images/misc/background.jpg" alt="back" style="height:300px;">
-  			<h1>Test Hot reload</h1>
-  			<script src="/reload/reload.js"></script>
-  		  `);
+  var data = req.app.get("appData");
+  var artWork = []; 	
+  data.speakers.forEach(function(item){
+  	artWork = artWork.concat(item.artwork);
+  });
+  console.log(1111111111111);
+  console.log(artWork);
+  console.log(1111111111111)
+
+  res.render('index',{
+	title: "Home",
+	page_id: "home",
+	art_work: artWork
+  });
 })
 
 module.exports = router;
